e067208acfa95597d3da6c0eaa76e54052e9eac8
jxdowd <18441262+jxdowd@users.noreply.github.com>
2022-03-16 02:13:01 -0500

    Update keymap.c

--- keyboards/rgbkb/mun/keymaps/jx/keymap.c
+++ keyboards/rgbkb/mun/keymaps/jx/keymap.c
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, _______, _______,
         _______, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, _______,
         _______, KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
-        _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
-        _______, _______, _______, TCH_TOG, _______, _______, _______, _______, ADJUST,  KC_PAGE_UP, KC_VOLD, KC_VOLU, KC_PAGE_DOWN,
+        _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
+        _______, _______, _______, TCH_TOG, _______, _______, _______, _______, ADJUST,  KC_PAGE_UP, KC_VOLD, KC_VOLU, KC_PAGE_DOWN, _______,
 
         _______, _______, _______, _______,                                                       _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_SAD, RGB_VAI, RGB_SAI, RESET,   _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
         _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
         _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, GAME,
-        _______, RGB_RMOD,_______, RGB_MOD, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY, COLEMAK,
+        _______, RGB_RMOD,_______, RGB_MOD, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY, _______,
 
         _______, _______, _______, _______,                                                       _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______
@@ -215,9 +215,6 @@ static void render_layer(void) {
         case _QWERTY:
             oled_write_ln_P(PSTR("QWRTY"), false);
             break;
-        case _COLEMAK:
-            oled_write_ln_P(PSTR("Colemk"), false);
-            break;
         case _GAME:
             oled_write_ln_P(PSTR("Game  "), false);
             break;
@@ -226,6 +223,15 @@ static void render_layer(void) {
             break;
         case _ADJUST:
             oled_write_ln_P(PSTR("Adjst"), false);
+            break;
+        case _LOWER:
+            oled_write_ln_P(PSTR("Lower"), false);
+            break;
+        case _RAISE:
+            oled_write_ln_P(PSTR("Raise"), false);
+            break;
+        case _ALTER:
+            oled_write_ln_P(PSTR("Alter"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
