987607566bd59c40babb732480d925db63b46174
jxdowd <jxdowd@gmail.com>
2025-12-26 12:23:54 -0600

    vbnm,

--- keyboards/preonic/keymaps/rud/keymap.c
+++ keyboards/preonic/keymaps/rud/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_VOLU, KC_VOLD, KC_END \
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_VOLU, KC_VOLD, KC_PGDN \
 ),
 
 /* Raise
